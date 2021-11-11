97b3050ab16aa9992615baf8c0f7447bb50097f0
andresvallejoz1991 <m3Tallicaeslaley>
2021-11-10 22:41:09 -0500

    Commit2

--- app/src/main/java/com/example/activity1/MainActivity.kt
+++ app/src/main/java/com/example/activity1/MainActivity.kt
@@ -8,12 +8,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Validar si una persona mayor de edad o no
         validarEdad()
+        //Presentar la tabla de multiplicar de n número de forma ascendente y descendente
         multiplicar()
+        //Mostrar le listado del paralelo de la materia y los subgrupos por proyecto
+        listado()
+        //Presentar las propiedades de un vehiculo utilizando clases
         propiedades()
+        //Ordenar de forma automatica un listado de números
+        ordenarNumeros()
+        //Validar una cedula
+        cedula()
     }
 
     private fun validarEdad(){
+        println("-------------------------------------------------------------------------------- ")
         val edad1 = 17
         if(edad1 >= 18){
             println("$edad1 años es mayor de edad")
@@ -22,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun multiplicar(){
+        println("-------------------------------------------------------------------------------- ")
         val n = 5
         println("La tabla de multiplicar de $n en forma ascendente es : ")
         for(i in 1..9){
@@ -35,12 +46,81 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun listado(){
+        println("-------------------------------------------------------------------------------- ")
+        val listas = listOf<String>("Andrés Vallejo","Brandon Vega","Frank Saca",
+            "Luis Quizphe", "Jordy Esparza","Erick Jaramillo","Edgar Guamo","Jefferson Cueva")
+
+        val ordenar = listas.sorted()
+        println("Listado de Estudiantes")
+        for (listas in ordenar){
+            println(listas)
+        }
+        val grupos = mutableMapOf("Andrés Vallejo" to "Turismo","Brandon Vega" to "Banca",
+            "Frank Saca" to "Turismo", "Luis Quizphe" to "Banca", "Jordy Esparza" to "Turismo",
+            "Erick Jaramillo" to "Banca", "Edgar Guamo" to "Turismo", "Jefferson Cueva" to "Banca")
+
+        val ordenGrupo = grupos.toSortedMap()
+        println("Listado de Estudiantes por Proyectos")
+        for (i in ordenGrupo ){
+            println("${i.key} - ${i.value}")
+        }
+    }
+
     private fun propiedades(){
-        val car =NewClass(arrayOf(NewClass.traction.Delantera),motor = "V8",tipo="Motorizado",capacidad="4 personas")
+        println("-------------------------------------------------------------------------------- ")
+        val car =NewClass(arrayOf(NewClass.traction.Delantera),motor = "V8",tipo="Motorizado",
+            capacidad="4 personas")
         car.vehiculo()
         println(car.motor)
         println(car.tipo)
         println(car.capacidad)
+    }
+
+    private fun ordenarNumeros(){
+        println("-------------------------------------------------------------------------------- ")
+        var numeros = arrayListOf<Int>(8,5,2,7,4,1,9,6,3)
+        var temporal = 0
+        println("Lista Inicial")
+        println("$numeros")
+        for(x in(0 until numeros.size)){
+            for (y in(0 until numeros.size)){
+                if (numeros[x]>numeros[y]){
+                    temporal = numeros[x]
+                    numeros[x] = numeros[y]
+                    numeros[y] = temporal
+                }
+            }
+        }
+        println("Lista de números ordenados")
+        println("$numeros")
+    }
+    private fun cedula(){
+        println("-------------------------------------------------------------------------------- ")
+        val cedula = arrayListOf<Int>(1,1,5,0,0,8,3,7,7,0)
+        var total =0
+        var contador=0
+        val numeroFinal = cedula.last()
+        for(x in cedula.indices){
+            contador=cedula[x]
+            if(x%2==0){
+                contador=(cedula[x]*2)
+                if (contador>9){
+                    contador = contador-9
+                }
+                total = total+contador
+            }else{
+                total = total+contador
+            }
+        }
+        total = total - numeroFinal
+        val primerNumero = total.toString().substring(0,1)
+        val decena = (primerNumero.toInt()+1)*10
+        if (decena-total==numeroFinal||decena-total==10){
+            println("La cedula -/ $cedula -/ si es valida.")
+        }else{
+            println("La cedula -/ $cedula -/ no es valida.")
+        }
     }
 
 }
